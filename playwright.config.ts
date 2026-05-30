6929637ca3509c2d2ec95b8b57accbf71965e0c9
mrjoe8888@gmail.com <mrjoe8888@gmail.com>
2026-05-31 01:08:45 +0800

    [Misc/PlaywrightConfig] removing unnecessary codes

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,11 +27,5 @@ export default defineConfig({
         ['html', { outputFolder: 'playwright-report', open: 'never' }],
         ['junit', { outputFile: 'test-results/junit.xml' }]
       ],
-
-    // reporter: [
-    //     ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    //     ['junit', { outputFile: 'test-results/junit.xml' }]
-    // ],
-
     outputDir: 'test-results/',
 })
