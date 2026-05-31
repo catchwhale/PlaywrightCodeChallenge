1bdc157f698933ae011c5ce1eb260e2d160f9c44
mrjoe8888@gmail.com <mrjoe8888@gmail.com>
2026-05-31 22:50:49 +0800

    [Misc] adding console log to read storageState json data

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,7 @@
 import { chromium, expect } from '@playwright/test';
 import { env } from '../config/env';
 import { LoginPage } from '../pages/LoginPage';
+import fs from 'fs';
 
 export async function login() {
     const browser = await chromium.launch();
@@ -13,5 +14,8 @@ export async function login() {
     await loginPage.verifysubURL(env.baseURL + '/web/index.php/dashboard/index');
 
     await page.context().storageState({ path: 'storageState.json' });
+    const content = fs.readFileSync('storageState.json', 'utf8');
+    console.log('File contents:');
+    console.log(content);
     await browser.close();
 }
